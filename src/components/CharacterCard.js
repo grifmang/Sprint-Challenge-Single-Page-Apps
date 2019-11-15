db25f7bd7836efd1036f1ef22f86821baf3ab7f5
Tim Griffith <grifmang@gmail.com>
2019-11-15 12:12:56 -0500

    completed data grab, welcome and character pages

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,22 @@
 import React from "react";
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+} from "reactstrap";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <Card>
+      <CardImg className='main-img' top width="100%" src={props.img} alt="Rick and Mortycharacter image" />
+      <CardBody>
+        <CardTitle>{props.name}</CardTitle>
+        <CardSubtitle>{props.gender}</CardSubtitle>
+        <CardSubtitle>{props.species}</CardSubtitle>
+        <CardSubtitle>{props.status}</CardSubtitle>
+      </CardBody>
+    </Card>
+  )
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,37 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://rickandmortyapi.com/api/character/')
+    .then(response => {
+      console.log(response.data.results);
+      setCharacter(response.data.results);
+    })
+    .catch(err => {console.log('the data was not returned', err)});
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>Character Data</h2>
+      {character.map(char => {
+        return (
+          <SearchForm />
+          <CharacterCard
+            key={char.id}
+            name={char.name}
+            img={char.image}
+            gender={char.gender}
+            species={char.species}
+            status={char.status} />
+        );
+      })}
     </section>
   );
 }
